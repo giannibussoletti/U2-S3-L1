848a7f98fca50c60a44a3598ddf51551b5eba49b
Gianni <BussolettiGianni@gmail.com>
2026-04-13 16:46:49 +0200

    Semi Final commit

---
+++ Esercizio/Animali/script.js
@@ -0,0 +1,71 @@
+const nomeAnimaleInput = document.getElementById("nome-input")
+const PadroneInput = document.getElementById("owner-input")
+const specieInput = document.getElementById("specie-input")
+const razzaInput = document.getElementById("razza-input")
+const formAnimal = document.getElementById("form-animal")
+const animalList = document.getElementById("rubrica")
+const paragraphOwner = document.createElement("p")
+let oldPet = {
+  nomeAnimale: "Fido",
+  padrone: "Francesco",
+  specie: "Cane",
+  razza: "Dalmata",
+}
+
+class AnimalData {
+  constructor(_nomeAnimale, _padrone, _specie, _razza) {
+    this.nomeAnimale = _nomeAnimale
+    this.padrone = _padrone
+    this.specie = _specie
+    this.razza = _razza
+  }
+  ageConfront = function (otherPet) {
+    if (this.padrone === otherPet.padrone) {
+      return (paragraphOwner.innerText = `${this.nomeAnimale} e ${otherPet.nomeAnimale} hanno lo stesso padrone`)
+    } else {
+      return (paragraphOwner.innerText = `Padrone Unico`)
+    }
+  }
+}
+
+formAnimal.addEventListener("submit", function (e) {
+  e.preventDefault()
+  const nomeAnimale = nomeAnimaleInput.value
+  const padrone = PadroneInput.value
+  const specie = specieInput.value
+  const razza = razzaInput.value
+  const newAnimal = new AnimalData(nomeAnimale, padrone, specie, razza)
+
+  const divCard = document.createElement("div")
+  divCard.classList.add("card", "my-3")
+
+  const divBody = document.createElement("div")
+  divBody.classList.add("card-body")
+
+  const h5 = document.createElement("h5")
+  h5.classList.add("card-title")
+  h5.innerText = newAnimal.nomeAnimale
+
+  const h6 = document.createElement("h6")
+  h6.classList.add("card-subtitle", "mb-2", "text-body-secondary")
+  h6.innerText = newAnimal.padrone
+
+  const parSpecie = document.createElement("p")
+  parSpecie.classList.add("card-text", "m-0")
+  parSpecie.innerText = `Specie: ${newAnimal.specie}`
+
+  const parRazza = document.createElement("p")
+  parRazza.classList.add("card-text")
+  parRazza.innerText = `Razza: ${newAnimal.razza}`
+
+  divCard.appendChild(divBody)
+  divBody.appendChild(h5)
+  divBody.appendChild(h6)
+  divBody.appendChild(parSpecie)
+  divBody.appendChild(parRazza)
+  divBody.appendChild(paragraphOwner)
+  animalList.appendChild(divCard)
+
+  oldPet = newAnimal
+  formAnimal.reset()
+})
---
+++ Esercizio/Persone/script.js
@@ -0,0 +1,72 @@
+const nomeInput = document.getElementById("nome-input")
+const cognomeInput = document.getElementById("cognome-input")
+const ageInput = document.getElementById("age-input")
+const locationInput = document.getElementById("location-input")
+const formPerson = document.getElementById("form-age")
+const rubrica = document.getElementById("rubrica")
+const paragraphAge = document.createElement("p")
+let oldUser = {
+  nome: "default",
+  cognome: "User",
+  age: 0,
+  location: "IT",
+}
+
+//
+
+class UserData {
+  constructor(_nome, _cognome, _age, _location) {
+    this.nome = _nome
+    this.cognome = _cognome
+    this.age = _age
+    this.location = _location
+  }
+  ageConfront = function (otherPerson) {
+    if (this.age > otherPerson.age) {
+      return (paragraphAge.innerText = `${this.nome} è più grande di ${otherPerson.nome}`)
+    } else if (this.age < otherPerson.age) {
+      return (paragraphAge.innerText = `${this.nome} è più piccolo di ${otherPerson.nome}`)
+    } else {
+      return (paragraphAge.innerText = `${this.nome} e ${otherPerson.nome} hanno la stessa età`)
+    }
+  }
+}
+
+formPerson.addEventListener("submit", function (e) {
+  e.preventDefault()
+  const nome = nomeInput.value
+  const cognome = cognomeInput.value
+  const age = ageInput.value
+  const location = locationInput.value
+  const newUser = new UserData(nome, cognome, age, location)
+
+  newUser.ageConfront(oldUser)
+
+  const divCard = document.createElement("div")
+  divCard.classList.add("card", "my-3")
+
+  const divBody = document.createElement("div")
+  divBody.classList.add("card-body")
+
+  const h5 = document.createElement("h5")
+  h5.classList.add("card-title")
+  h5.innerText = `${newUser.nome} ${newUser.cognome}`
+
+  const h6 = document.createElement("h6")
+  h6.classList.add("card-subtitle", "mb-2", "text-body-secondary")
+  h6.innerText = newUser.location
+
+  const p = document.createElement("p")
+  p.classList.add("card-text")
+  p.innerText = `età: ${newUser.age}`
+
+  divCard.appendChild(divBody)
+  divBody.appendChild(h5)
+  divBody.appendChild(h6)
+  divBody.appendChild(p)
+  divBody.appendChild(paragraphAge)
+  rubrica.appendChild(divCard)
+
+  oldUser = newUser
+  formPerson.reset()
+})
